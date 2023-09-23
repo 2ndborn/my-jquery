a924e2bc7b1f1b998d6614fafbc2a36643e1675c
2ndborn <>
2023-09-23 07:19:22 +0000

    2nd commit

--- script.js
+++ script.js
@@ -17,4 +17,24 @@ $(document).ready(function () {
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
     });
+    $('p').click(function () {
+        $('p').css('color', 'red');
+    });
+    $('h2').hover(function () {
+        $('h2').css('background-color', 'lightblue');
+    });
+    $("#hr_html").hover(function () {
+        $("#hr_mysql").css('font-size', '1em');
+        $("#hr_python").css('font-size', '1em');
+        $("#hr_jquery").css('font-size', '1em');
+        $("#hr_django").css('font-size', '1em');
+        $("#hr_css").css('font-size', '1em');
+        $("#hr_html").css('font-size', '2em');
+    });
+    $('.bottom_button').mouseenter(function () {
+        $('body').css('background-color', 'black');
+    });
+    $('.bottom_button').mouseleave(function () {
+        $('body').css('background-color', 'grey');
+    });
 }); 
